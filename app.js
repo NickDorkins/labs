e7f6b948692cca9d765b5c0718a42bc82b2939a4
NickDorkins <nick.dorkins@gmail.com>
2020-08-27 23:06:35 -0700

    rendered chart

--- app.js
+++ app.js
@@ -110,10 +110,134 @@ function eventHandler(e) {
   }
 }
 
+function renderChart() {
+  var clicksArray = [];
+  var viewedArray = [];
+  var imageArray = [];
+
+  for (var i = 0; i < imgArray.length; i++){
+    clicksArray.push(imgArray[i].clicked);
+    viewedArray.push(imgArray[i].viewed);
+    imageArray.push(imgArray[i].name);
+  }
+
+  var chartObject = {
+    type: 'horizontalBar',
+    data: {
+      labels: imageArray,
+      datasets: [{
+        label: '# of Votes',
+        data: clicksArray,
+        backgroundColor: [
+          'rgba(150, 206, 15, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(100, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(150, 206, 15, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(100, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(150, 206, 15, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(150, 206, 15, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(100, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(150, 206, 15, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(100, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+
+          'rgba(255, 159, 64, 1)'
+        ],
+        hoverBackgroundColor: 'teal',
+        borderWidth: 1
+      }, {
+        label: '# of Views',
+        data: viewedArray,
+        backgroundColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)'
+
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(150, 206, 15, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(100, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(150, 206, 15, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(100, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+
+          'rgba(255, 159, 64, 1)'
+        ],
+        hoverBackgroundColor: 'pink',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      responsive: false,
+    }
+  };
+
+  var ctx = document.getElementById('myChart').getContext('2d');
+
+  var myChart = new Chart(ctx, chartObject); //eslint-disable-line
+}
+
 imgElOne.addEventListener('click', eventHandler);
 imgElTwo.addEventListener('click', eventHandler);
 imgElThree.addEventListener('click', eventHandler);
 
 renderImages();
+renderChart();
 
 
